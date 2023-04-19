f92254e69f2b2f59bd0e2ad73a4b2e736baf8ab7
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2023-04-19 16:19:42 -0500

    _____

--- Etapa_2/main.py
+++ Etapa_2/main.py
@@ -10,17 +10,17 @@ from fastapi.responses import HTMLResponse
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
+@app.get("/")# Interface principal
 def read_root():
    return {"Hello": "World"}
 
 
-@app.get("/doc1/",response_class=HTMLResponse)
+@app.get("/doc1/",response_class=HTMLResponse)# va a motrar doc1
 def doc2(request: Request):
    context ={'request':request}
    return templates.TemplateResponse("doc1.html",context)
 
-@app.get("/doc2/",response_class=HTMLResponse)
+@app.get("/doc/",response_class=HTMLResponse) #va a mostrar doc 2 
 def doc1(request: Request):
    context ={'request':request}
    return templates.TemplateResponse("doc2.html",context)
