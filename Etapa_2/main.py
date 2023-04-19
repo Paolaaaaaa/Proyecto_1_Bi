eeb3d45f05e195723317e5319f02a84eea8392f5
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2023-04-19 01:28:50 -0500

    ____doc

--- Etapa_2/main.py
+++ Etapa_2/main.py
@@ -1,13 +1,30 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
+
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+
 
 app = FastAPI()
-
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def read_root():
    return {"Hello": "World"}
+
+
+@app.get("/doc1/",response_class=HTMLResponse)
+def doc2(request: Request):
+   context ={'request':request}
+   return templates.TemplateResponse("doc1.html",context)
+
+@app.get("/doc2/",response_class=HTMLResponse)
+def doc1(request: Request):
+   context ={'request':request}
+   return templates.TemplateResponse("doc2.html",context)
+   
 
 
 @app.get("/items/{item_id}")
