58fbfac2f7b15db216971c00cbde760cb0b630c7
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2023-04-20 01:54:39 -0500

    __avance

--- Etapa_2/DataModel.py
+++ Etapa_2/DataModel.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
-
+from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline
+from joblib import dump
 class DataModel(BaseModel):
     review_es: str
 
@@ -7,4 +9,24 @@ class DataModel(BaseModel):
 def columns(self):
         return ["review_es"]
 
+
+def generate_pipeLine():
+
+    pipeline = Pipeline(
+        [
+            ('scaler', StandardScaler()),
+            ('model', LinearRegression())
+        ]
+    )
+    pipeline.fit(x_train_m2, y_train_m2)
+    dump(pipeline, 'modelo.joblib')
+
+
+
+def use_pipeline(movie):
+    filename = 'modelo.joblib'
+    df_recent = pd.read_csv('./data/'+movie+'.csv', sep=',', encoding = 'utf-8') # Lectura de los datos recientes
+    pipeline = joblib.load(filename)
+    y_predicted =  pipeline.predict(df_recent)
+    return y_predicted
 
